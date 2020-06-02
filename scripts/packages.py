0ab7b10cb27268699651d26f3917277aec017f73
mariamsm1 <61454614+mariamsm1@users.noreply.github.com>
2020-06-02 16:27:51 +0200

    Create packages.py

---
+++ scripts/packages.py
@@ -0,0 +1,27 @@
+#to untar downloaded databases
+import tarfile
+
+#to download databases
+import wget
+
+#to gunzip downloaded databases
+import gzip
+
+#to unzip downloaded databases
+import zipfile
+from zipfile import ZipFile
+
+#to read and write csv files
+import csv
+import pandas as pd #also used for reading htmls
+
+#for webscraping
+import bs4
+from bs4 import BeautifulSoup
+
+#for reading .owl format databases
+from owlready2 import *
+
+#for downloading .obo formats
+import goatools
+from goatools.obo_parser import GODag
