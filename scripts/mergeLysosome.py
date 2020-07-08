451e4ddafd891832c251ebfcbefd6d5910891790
mariamsm1 <61454614+mariamsm1@users.noreply.github.com>
2020-07-08 13:10:21 +0200

    Update mergeLysosome.py

--- scripts/mergeLysosome.py
+++ scripts/mergeLysosome.py
@@ -579,27 +579,31 @@ with open('oversyn', 'r') as over, open('Clean_Lysosome_Data', 'w') as out2:
         
 #map lysosome data to uniprot and take the reviewed entries
 
-with open('Reviewed_Final_LysosomeMappedData') as clean, open('Final_Clean_Lysosome_Data', 'w') as out:
-     for line in clean:
+lys_dict = {}
+with open('Reviewed_ALL_separated_Lysosome') as rev, open('Final_Clean_Lysosome_Data', 'w') as out:
+    print('Lys_Uniprot','Lys_Symbol','Lys_Synonym','Lys_ProteinName','Lys_Organism', sep = '\t', file = out)
+    for line in rev:
         if not 'your' in line:
             line = line.rstrip()
-            line=line.split('\t')
+            line = line.split('\t')[0:9]
+            line = ["nan" if x == '' else x for x in line]
+            line = '\t'.join(line)
+            #split again because now the empty fields that were tabs are replaced by 'nan'
+            line = line.split('\t')
             uniprot = line[0]
-            gsyn = line[5]
-            symbol = gsyn.split(' ')[0]
-            if symbol == '':
-                symbol = 'nan'
-            else:
-                symbol = symbol
-            synonym = gsyn.split(' ')[1:]
-            synonym = ','.join(synonym)
-            if synonym == '':
-                synonym = 'nan'
-            else:
-                synonym = synonym
-            protname = line[4]
-            protname = protname.split('(EC ')[0]
-            protname = protname.split('[Cleaved ')[0]
-            org = line[6]
-            print(uniprot,symbol,synonym,protname,org, sep = '\t', file = out) 
+            uniprot = uniprot.split(',')
+            for element in uniprot:
+                lys_dict[element] = {}
+                symbol = line[5]
+                lys_dict[element]['symbol'] = symbol
+                synonym = line[6].replace(' ', ',')
+                lys_dict[element]['synonym'] = synonym
+                protname = line[7]
+                protname = protname.split('(EC ')[0]
+                protname = protname.split('[Cleaved ')[0]
+                lys_dict[element]['protname'] = protname
+                organism = line[8]
+                lys_dict[element]['org'] = organism
+    for key in lys_dict:
+        print(key, lys_dict[key]['symbol'], lys_dict[key]['synonym'], lys_dict[key]['protname'], lys_dict[key]['org'], sep = '\t', file = out)
 #------------------------------    
